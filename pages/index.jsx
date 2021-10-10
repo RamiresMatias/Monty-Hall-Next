e9c3cd0452f8f849c23560bd0b2f8e51ff573d96
RamiresMatias <ramiresmatias20000@gmail.com>
2021-10-10 14:24:29 -0300

    Add typescript e lógica de renderização

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,24 @@
-import Presente from "../components/Presente"
 import Porta from "../components/Porta"
+import {useState} from "react"
+import {atualizarPortas, criarPortas} from "../functions/portas"
 
 
 export default function Home() {
+
+  const [portas, setPortas] = useState(criarPortas(4,3));
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return <Porta key={porta.numero} value={porta} 
+        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))
+        }
+      />
+    })
+  }
+
   return (
     <div style={{display: 'flex'}}>
-      <Porta selecionada/>
-      <Porta/>
+      {renderizarPortas()}
     </div>
   )
 }
